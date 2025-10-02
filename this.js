8f8b71533b5f800cf949cb236354280befc09f19
TheRemonDev <riyanahmed3536@gmail.com>
2025-10-02 21:36:59 +0100

    explain

--- this.js
+++ this.js
@@ -13,3 +13,15 @@ const solim = new Person("Solim" , 45);
 console.log(solim);
 
 solim.sleep()
+
+// Encapsulation কী?
+// Encapsulation মানে হলো:
+
+// Object-এর data (property) এবং method-কে এক জায়গায় লুকানো বা গঠন করা।
+// অন্য object বা user সরাসরি সেই data পরিবর্তন করতে পারবে না, বরং method-এর মাধ্যমে access করতে হবে।
+// উদ্দেশ্য:
+
+// Data hide করা → কেউ সরাসরি property পরিবর্তন করতে পারবে না।
+// Security এবং integrity বজায় রাখা।
+// Object-এর behavior ঠিক রাখা।
+// **rivate property করার জন্য # ব্যবহার করা হয়।
