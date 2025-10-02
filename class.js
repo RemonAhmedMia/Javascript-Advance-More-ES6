0b15b7816d0bd1208885e214fb70614cd1cef8be
TheRemonDev <riyanahmed3536@gmail.com>
2025-10-02 19:57:51 +0100

    constrauctor-example

--- class.js
+++ class.js
@@ -44,3 +44,30 @@ const s2 = new Student("Karim", 22);
 
 s1.showInfo(); // Name: Rahim, Age: 20
 s2.showInfo(); // Name: Karim, Age: 22
+
+
+// ================Constructor কী?
+
+// JavaScript এ constructor হলো একটা special method, যেটা class এর ভেতরে define করা হয়।
+
+// এর কাজ হলো object বানানোর সময় প্রথমে call হওয়া।
+
+// constructor সাধারণত object এর initial value (প্রাথমিক মান) সেট করার জন্য ব্যবহার করা হয়।
+
+class Student {
+  constructor(name, age) {
+    this.name = name; // object এর name property
+    this.age = age;   // object এর age property
+  }
+}
+
+const s = new Student("Rahim", 20);
+console.log(s); // Student { name: "Rahim", age: 20 }
+// এখানে constructor-এর কাজ হচ্ছে, যখন new Student("Rahim", 20) লেখা হলো, তখন ,constructor চালু হলো,
+// আর this.name = name এবং this.age = age এর মাধ্যমে object এ মান বসিয়ে দিলো।
+
+
+// ===============this কী?
+
+// this হলো একটা keyword যেটা object-এর ভেতরে সেই object-কে বোঝায়।
+// মানে, this সবসময় বর্তমান object কে নির্দেশ করে।
